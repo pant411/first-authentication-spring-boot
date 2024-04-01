784f4c2c4dabe952d173e06b38e7b3da5eb2d8f6
pant411 <pantchanit2561@hotmail.com>
2024-04-01 17:03:42 +0700

    feat: login dto

---
+++ src/main/java/com/project/authapi/auth/dtos/LoginDto.java
@@ -0,0 +1,17 @@
+package com.project.authapi.auth.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginDto {
+  @NotEmpty(message = "Email address is required")
+  @Email(message = "Invalid email address")
+  private String username;
+
+  @NotEmpty(message = "Password is required")
+  @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+  private String password;
+}
